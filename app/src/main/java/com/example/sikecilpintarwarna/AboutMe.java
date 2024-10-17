fc81d8e95a10809eed5652c96863a05ba82a9132
castreld <hulukotak@gmail.com>
2024-10-17 11:45:39 +0700

    FOURTH COMMIT TADI ADA YANG SALAH LAGI WKWKKW

--- app/src/main/java/com/example/sikecilpintarwarna/AboutMe.java
+++ app/src/main/java/com/example/sikecilpintarwarna/AboutMe.java
@@ -1,6 +1,10 @@
 package com.example.sikecilpintarwarna;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +19,29 @@ public class AboutMe extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_about_me);
+
+        Button menuButton = findViewById(R.id.menuButton), webButton = findViewById(R.id.webButton);
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AboutMe.this, MainActivity.class);
+
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        webButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://firaasraihansyah.my.id/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
